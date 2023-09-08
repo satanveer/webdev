90c7dc001042a9db52623e6f89a81b86d100a593
satanveer <satanveersingh@gmail.com>
2023-09-09 00:29:04 +0530

    learning Java Script

---
+++ JS-CWH/practice.js
@@ -0,0 +1,29 @@
+/*let marks = {
+    harry:90,
+    monika:15,
+    arnav:16,
+    tanveer:99
+}
+for(let i=0;i<Object.keys(marks).length;i++){
+    console.log("The marks of " + Object.keys(marks)[i] + " are "+marks[Object.keys(marks)[i]])
+}
+*/
+
+
+/*
+let marks = {
+    harry:90,
+    monika:15,
+    arnav:16,
+    tanveer:99
+}
+for(let i in marks){
+    console.log(i+marks[i])
+}
+*/
+
+const mean =(a,b,c,d)=>{
+    return (a+b+c+d)/4
+}
+
+console.log(mean(4,5,6,7))
