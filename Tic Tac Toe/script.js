e3e1d355fae198b914ed537ab73acb06262977b3
satanveer <satanveersingh@gmail.com>
2023-10-06 18:26:22 +0530

    made the tic tac toe game

--- Tic Tac Toe/script.js
+++ Tic Tac Toe/script.js
@@ -2,6 +2,7 @@ let music= new Audio("images-sounds/music.mp3")
 let audioTurn= new Audio("images-sounds/ting.mp3")
 let turn = "X"
 let isgameover=false
+let musicSkipity= new Audio("images-sounds/skipity-dip-dop.mp3")
 
 
 
@@ -14,20 +15,24 @@ const changeTurn=()=>{
 //Function to check win
 
 const checkWin=()=>{
+    let boxtext=document.getElementsByClassName('boxtext')
     let wins=[
-        [0,1,2]
-        [3,4,5]
-        [6,7,8]
-        [0,3,6]
-        [1,4,7]
-        [2,5,8]
-        [0,4,8]
-        [2,4,6]
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
     ]
     wins.forEach(e=>{
         if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[1]].innerText === boxtext[e[2]].innerText) && boxtext[e[0]].innerText !== ""){
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText + "won"
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " won"
             isgameover=true;
+            document.getElementById('modi-img').style.width="17vw"
+            document.getElementById('modi-img').style.height="20vh"
+
+            musicSkipity.play();
         }
     })
 
@@ -45,11 +50,25 @@ Array.from(boxes).forEach(element=>{
             audioTurn.play();
             checkWin();
             if(!isgameover){
-                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
             }
 
 
         }
     })
+
+})
+reset.addEventListener('click',()=>{
+    let boxtexts=document.querySelectorAll(".boxtext");
+    Array.from(boxtexts).forEach(element=>{
+        element.innerText="";
+    })
+    turn="X"
+    isgameover=false;
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.getElementById('modi-img').style.width="0vw"
+    musicSkipity.pause();
+    musicSkipity.currentTime=0;
+
 
 })
