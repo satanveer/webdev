4cde240298accfd34692547370b8e29b184a0efc
satanveer <satanveersingh@gmail.com>
2023-10-06 17:14:00 +0530

    started making a tic tac toe web app

---
+++ portfolioWebsiteV2/script.js
@@ -0,0 +1,8 @@
+let github_url="https://github.com/satanveer";
+document.getElementById("github_icon").onclick=function(){
+    window.open(github_url);
+}
+let linkldn_url="https://www.linkedin.com/in/tanveer-singh-2b9948284/";
+document.getElementById("linkldn_icon").onclick=function(){
+    window.open(linkldn_url);
+}
