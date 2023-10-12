584189da4846879e967dcc924d9d8232d0bab3ac
satanveer <satanveersingh@gmail.com>
2023-10-12 23:49:15 +0530

    made a to do list

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,1 +1,34 @@
-let textBox = ();
+function addTask() {
+    const inputBox = document.getElementById("task-input");
+    
+    if (inputBox.value === '') {
+        alert("Add a task please.");
+    } 
+    else {
+        const newButton=document.createElement('button')
+        newButton.classList.add('done')
+        newButton.textContent="DONE"
+        newButton.addEventListener("click",function(){
+            newDiv.remove()
+        })
+        
+        const taskAll= document.getElementsByClassName('taskall')[0]
+        var numberOfChildren=taskAll.getElementsByTagName('*').length
+
+        const newSpan=document.createElement('span')
+        newSpan.classList.add('task-list')
+        newSpan.textContent=inputBox.value
+       
+        const newDiv=document.createElement('div')
+        newDiv.classList.add("tasks")
+
+        
+        taskAll.style.opacity="1"
+        
+        
+
+        taskAll.appendChild(newDiv)
+        newDiv.appendChild(newSpan)
+        newDiv.appendChild(newButton)
+    }
+}
